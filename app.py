33849b5edb4c2b82bb1ec2358af12463cea54828
Kavya Agarwal <kavyaag1617@gmail.com>
2026-03-22 15:22:48 +0530

    app upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import freecurrencyapi
+
+client = freecurrencyapi.Client('fca_live_pNMM3Xth7ecjljuqJJrVPwP4fxAM9TehW89jcj6g')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Server is running 🚀"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+    cf = fetch_conversion_factor(source_currency, target_currency)
+
+    final_amount = round(amount * cf, 2)
+
+    return jsonify({
+        "fulfillmentText": f"{amount} {source_currency} is {final_amount} {target_currency}"
+    })
+
+def fetch_conversion_factor(source, target):
+    result = client.latest(base_currency=source, currencies=[target])
+    return result['data'][target]
+
+if __name__ == "__main__":
+    app.run()
